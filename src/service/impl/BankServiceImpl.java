e385a1fafafbca5f4be2791e783759b0bd762392
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-16 20:21:46 +0530

    - Added the logic of deposit.

--- src/service/impl/BankServiceImpl.java
+++ src/service/impl/BankServiceImpl.java
@@ -1,9 +1,13 @@
 package service.impl;
 
 import domain.Account;
+import domain.Transaction;
+import domain.Type;
 import repository.AccountRepository;
+import repository.TransactionRepository;
 import service.BankService;
 
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
@@ -12,6 +16,7 @@ import java.util.stream.Collectors;
 public class BankServiceImpl implements BankService {
 
     private final AccountRepository accountRepository = new AccountRepository();
+    private final TransactionRepository transactionRepository = new TransactionRepository();
 
     @Override
     public String openAccount(String name, String email, String accountType) {
@@ -34,6 +39,14 @@ public class BankServiceImpl implements BankService {
         return accountRepository.findAll().stream()
                 .sorted(Comparator.comparing(Account::getAccountNumber))
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deposit(String accountNumber, Double amount, String deposit) {
+        Account account = accountRepository.findByAccountNumber(accountNumber).orElseThrow(() -> new RuntimeException("Account not found " + accountNumber));
+        account.setBalance(account.getBalance() + amount);
+        Transaction transaction = new Transaction(UUID.randomUUID().toString(), Type.DEPOSIT, account.getAccountNumber(), amount, LocalDateTime.now(), deposit);
+        transactionRepository.add(transaction);
     }
 
     private String getAccountNumber() {
