16826be60d5e8e66c47c86a6a03f5a3a9b5dbd78
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-26 13:04:43 +0530

    - Completed the "statement" functionality in the Main class.

--- src/app/Main.java
+++ src/app/Main.java
@@ -88,7 +88,9 @@ public class Main {
     private static void statement(Scanner scanner, BankService bankService) {
         System.out.println("Account number : ");
         String accountNumber = scanner.nextLine().trim();
-        bankService.getStatement(accountNumber);
+        bankService.getStatement(accountNumber).forEach(transaction -> {
+            System.out.println(transaction.getTimestamp() + " | " + transaction.getType() + " | " + transaction.getAmount() + " | " + transaction.getNote());
+        });
     }
 
     private static void listAccounts(Scanner scanner, BankService bankService) {
