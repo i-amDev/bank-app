132439ba776292cd938a7f3aa00b0bdc40dbf4e9
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-26 13:57:27 +0530

    - Created the "CustomerRepository".

---
+++ src/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import domain.Customer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerRepository {
+
+    private final Map<String, Customer> customersById = new HashMap<>();
+
+
+}
